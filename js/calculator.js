b7d499a156f20a9f1a154271f40e6236f02fef16
sjackson01 <stevenpjackson0@gmail.com>
2020-03-05 10:49:01 -0600

    Modify (calculator.php>Hunk 1) : Display errors by manipulating the DOM.

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,10 @@
 // Script 15.5 - calculator.js
 // This script handles and validates the form submission
-// Do something when the document is ready
 
 $(function() {
+
+    // Hide all error messages
+    $('.errorMessage').hide();
 
     // Assign an event handler to the form
     $('#calculator').submit(function(){
@@ -16,35 +18,62 @@ $(function() {
             // Get the quantity
             quantity = $('#quantity'). val();
 
-        }else{
-            // Alert the user:
-            alert('Please enter a valid quantity!');
+            // Clear an error, if one existed:
+            $('#quantityP').removeClass('error');
+     
+            //Hide the error message, if it was vidible: 
+            $('#quantityError').hide();
+        }else{ //Invalide Quantity
+
+           // Add an error class:
+           $('#quantityP').addClass('error');
+    
+           // Show the error message:
+           $('#quantityError').show();
+            
         }
 
         // Validate the price:
         if($('#price').val() > 0 ) {
+
             price = $('#price').val();
+
+            $('#priceP').removeClass('error');
+
+            $('#priceError').hide();
         }else{
-            alert('Please enter a valid price!');
+
+            $('#priceP').addClass('error');
+
+            $('#priceError').show();
         }
 
         // Validate the tax 
         if ($('#tax').val() > 0){
+
             tax = $('#tax').val();
+
+            $('#taxP').removeClass('error');
+
+            $('#taxError').hide();
         }else{
-            alert('Please enter a valid tax!');
+            $('#taxP').addClass('error');
+
+            $('#taxError').show();
         }
 
-        // If appropriate perfrom the calculations: 
+        // If appropriate, perform the calculations: 
         if (quantity && price && tax){
 
             total = quantity * price;
             total += total * (tax/100);
 
-            // Display the results
-            alert('The toal is $' + total);
-        }
+            // Display the results:
+            $('#results').html('The total is <strong>$' + total + '</strong>.');
 
+        }
+        // Return false to prevent an actual form submission:
+        return false; 
     }); // End of form submission. 
 
 }); // End of document ready. 
